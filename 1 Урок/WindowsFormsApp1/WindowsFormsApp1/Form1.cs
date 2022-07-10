31611327fff7e44ec70bed2563e3446db2f1c7bf
KotyaRa2001 <85042151+KotyaRa2001@users.noreply.github.com>
2022-07-10 15:35:20 +0300

    Урок 1

---
+++ 1 Урок/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World";
+            label2.Visible = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хм, а зачем ты нажал на меня то?",
+                "А зочем?",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Question);
+            this.Close();
+        }
+    }
+}
