cc4623adf55c91a9a6b9d8bec1e0ecf8caa3d815
KotyaRa2001 <85042151+KotyaRa2001@users.noreply.github.com>
2022-07-10 15:35:58 +0300

    Урок 2-3

---
+++ 2-3 Урок/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                string password = Form2.Show(
+                "Аутентификация пользователя",
+                "Введите Ваш пароль:");
+                if (password != "12345")// Плохой способ хранения пароля
+                {
+                    MessageBox.Show(
+                    "Извините, но Вам не разрешено\n" // Сообщение
+                    + "пользоваться этой суперпрограммой!!!",
+                    "Неверный пароль", // Заголовок окна
+                    MessageBoxButtons.OK, // Кнопка OK
+                    MessageBoxIcon.Stop);// Критическая иконка
+                    this.Close();
+                }
+            }
+
+
+        }
+
+
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+    }
+}
